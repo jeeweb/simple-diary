c2d2f5c0eaa51710b031ee43938d03cca8bc386a
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-03-24 00:29:07 +0900

    diaryList 추가 - diaryItem 컴포넌트 분리작업

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "홍길동1",
+    content: "하이1",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "홍길동2",
+    content: "하이2",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "홍길동3",
+    content: "하이3",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "홍길동4",
+    content: "하이4",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
